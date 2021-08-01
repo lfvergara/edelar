fe37c0115af551619a24a11b781edcb618ca008d
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-01 20:53:33 -0300

    Consulta de Deuda

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -28,8 +28,8 @@ class SitioController {
 
 	function rse() {
 		$select = "rse.denominacion AS TITULO, rse.epigrafe AS EPIGRAFE";
-		//$from = "rse rse LIMIT 3";
-		//$rse_collection = CollectorCondition()->('RSE', NULL, 4, $from, $select);
+		$from = "rse rse LIMIT 3";
+		$rse_collection = CollectorCondition()->get('RSE', NULL, 4, $from, $select);
 		print_r($rse_collection);exit;
 		$this->view->rse();
 	}
