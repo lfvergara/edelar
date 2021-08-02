eb73e1c97b990866c0f28be2ffe9eaaafb8ecd75
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-01 21:20:18 -0300

    Consulta de Deuda

--- core/helpers/files.php
+++ core/helpers/files.php
@@ -1,14 +1,32 @@
 <?php
-$file = filter_input(INPUT_GET, "f");
- 
-if(!is_null($file)) {
-	$archivo = URL_DOCUMENTACION . "{$file}";
-	if(file_exists($archivo)) {
-		$finfo = finfo_open(FILEINFO_MIME_TYPE);
-		$mime = finfo_file($finfo, $archivo);
-		finfo_close($finfo);
-		header("Content-Type: $mime");
-		readfile($archivo);
-	}
+$file = (isset($_GET['f'])) ? $_GET['f'] : '';
+$ids = explode("_", $file);
+
+$objeto = $ids[0];
+$propiedad = $ids[1];
+$url = (isset($ids[2])) ? $ids[2] : "";
+$archivo = URL_PRIVATE . "{$objeto}/{$propiedad}/{$url}";
+
+if(file_exists($archivo)) {
+	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$mime = finfo_file($finfo, $archivo);
+	finfo_close($finfo);
+	header("Content-Type: {$mime}");
+	ob_end_clean();
+	$imagen = readfile($archivo);
+} elseif ($objeto == 0) {
+	$archivo = URL_PRIVATE . "common/imagenes/image_pdf.png";
+	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$mime = finfo_file($finfo, $archivo);
+	finfo_close($finfo);
+	header("Content-Type: $mime");
+	$imagen = readfile($archivo);
+} else {
+	$archivo = URL_PRIVATE . "common/imagenes/no_image.png";
+	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$mime = finfo_file($finfo, $archivo);
+	finfo_close($finfo);
+	header("Content-Type: $mime");
+	$imagen = readfile($archivo);
 }
 ?>
