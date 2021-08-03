badc49513e5aba739fab60d0dc5f22529a6fb16d
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-02 23:21:36 -0300

    Get Deuda

--- tools/getDeuda.php
+++ tools/getDeuda.php
@@ -6,19 +6,20 @@ require_once "common/libs/sha256encript/AESEncrypter.php";
 
 class getDeuda {
 	function getDeuda($metodo, $valor) {
-		$aes= new AESEncrypter();
 		$ip= '144.217.5.1';
 		$usuario = 'PROVEEDORWEB_7894265917';
 		$clave = 'TY3tTnOj7QTf9HDgsFg9KOYT7k5F85T9VcrjFtp9';
 		$secretKey = 'PROVEEDOR_cwcFscHTl6UD9MWsnqUHE17wxXqfvHLmaDC9PfHl_WEB'; 
 		$ente = 'GtO9lF1FsExXcrSgq9Kpw3stFkct5mt_E007';
 		//Generacion de firma
+		$sha256= new SHA256Encript();
 		$firma = $sha256->Generate($ip, $secretKey, $ente, $usuario, $clave);
 
 		//Definicion de metodo y valor
 		//$metodo = 'nis';
 		//$valor = '5050150';
 		//Creacion de array de envio con los datos encriptados
+		$aes= new AESEncrypter();
 		$data = array();
 		$data ['firma']= $firma;
 		$data ['usuario']= $aes-> EncrypString($usuario, $secretKey);
