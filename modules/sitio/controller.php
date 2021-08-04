0cdb35709dea565d35c1281b93947163499e9d40
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-04 13:29:28 -0300

    Deuda

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -128,6 +128,7 @@ class SitioController {
 		}
 
 		$gui = $this->view->imprimir_factura_ajax($obj_deuda, $factura_id, $suministro_id);
+		print_r($gui);exit;
 		$mipdf = new DOMPDF();
         $mipdf->set_paper("A4", "portrait");
         $mipdf->load_html($gui);
