fc116337bbec9afeeab98a2e858b9db097b02ee7
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-04 12:07:47 -0300

    Deuda

--- modules/sitio/view.php
+++ modules/sitio/view.php
@@ -135,7 +135,8 @@ class SitioView extends View {
 
 	function consultar_factura_ajax($obj_deuda, $factura_id, $suministro) {
 		$gui = file_get_contents("static/modules/sitio/consultar_factura_ajax.html");
-		$obj_deuda = $this->set_dict($obj_deuda);		
+		$obj_deuda = $this->set_dict($obj_deuda);
+		print_r($obj_deuda);exit;
 		$render = $this->render($obj_deuda, $gui);
 		$render = str_replace('{wsfactura-id}', $factura_id, $render);
 		$render = str_replace('{wssuministro-id}', $suministro, $render);
