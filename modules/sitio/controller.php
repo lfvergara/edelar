11b06bdb5d5a95189e3335f1f608b336d02d1dce
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-05 19:42:29 -0300

    Agrego funcionalidades de trabjar en edelar

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -71,7 +71,7 @@ class SitioController {
 
 	function trabajaedelar() {
 		$select = "ain.areainteres_id AS AREINTID, ain.denominacion AS DENOMINACION";
-		$from = "areainteres ain ORDER BY ain.denominacion ASC";
+		$from = "areainteres ain";
 		$areainteres_collection = CollectorCondition()->get('AreaInteres', NULL, 4, $from, $select);
 		print_r($areainteres_collection);exit;
 		$this->view->trabajaedelar($areainteres_collection);
