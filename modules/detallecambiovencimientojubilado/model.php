146a2260cb3049b6120948dcaa1e2257b9793f9e
Federico Vergara <hu.ce.ro@gmail.com>
2021-08-08 01:00:38 -0300

    Agrego objeto para gestión comercial Cambio de Vencimiento de Jubilados

---
+++ modules/detallecambiovencimientojubilado/model.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'modules/gestioncomercial/model.php';
+
+
+class DetalleCambioVencimientoJubilado extends StandardObject {
+	
+	function __construct(GestionComercial $gestioncomercial=NULL) {
+		$this->detallecambiovencimientojubilado_id = 0;
+		$this->numero_tramite = 0;
+		$this->dia_vencimiento = 0;
+		$this->termino_condiciones = 0;
+		$this->fecha_termino_condiciones = '';
+		$this->ip = '';
+		$this->so = '';
+		$this->tipo = '';
+		$this->detalle = '';
+		$this->gestioncomercial = $gestioncomercial;
+	}
+}
+?>
