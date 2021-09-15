612261273f7670e5a1950181fa8a348367b6bd15
Federico Vergara <hu.ce.ro@gmail.com>
2021-09-15 12:16:41 -0300

    Arreglo de renderizado movil

--- tools/getDeuda.php
+++ tools/getDeuda.php
@@ -34,6 +34,7 @@ class getDeuda {
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		$response = curl_exec($ch);
+		print_r($response);exit;
 		return $response;
 	}
 }
