1bbadc37731be063809709cdaba67b2468dbfb76
Federico Vergara <hu.ce.ro@gmail.com>
2021-10-04 12:07:25 -0300

    Oficina Virtual

--- modules/clienteusuario/controller.php
+++ modules/clienteusuario/controller.php
@@ -39,17 +39,17 @@ class ClienteUsuarioController {
 		$gender = "M";
 		$questionary = 0;
 
-		$array = array('web:matrix'=>$matrix,
-					   'web:user'=>$user,
-					   'web:password'=>$password,
-					   'web:sector'=>$sector,
-					   'web:sucursal'=>$sucursal,
-					   'web:documentNumber'=>$documentNumber,
-					   'web:gender'=>$gender,
-					   'web:questionary'=>$questionary);
+		$array = array('matrix'=>$matrix,
+					   'user'=>$user,
+					   'password'=>$password,
+					   'sector'=>$sector,
+					   'sucursal'=>$sucursal,
+					   'documentNumber'=>$documentNumber,
+					   'gender'=>$gender,
+					   'questionary'=>$questionary);
 
 		$client = new SoapClient("https://online.org.veraz.com.ar/WsIDValidator/services/idvalidator?wsdl");
-		$result = $client->__soapCall("obtenerPreguntas", $array);
+		$result = $client->__soapCall("ObtenerPreguntas", $array);
 		print_r($result);exit;
 
 		/*
