c4660afa536534f2ac1304a22abd97fc80699a94
Federico Vergara <hu.ce.ro@gmail.com>
2021-10-05 10:17:12 -0300

    Turnero Online

--- modules/sitio/view.php
+++ modules/sitio/view.php
@@ -536,7 +536,8 @@ class SitioView extends View {
 
 	function gestion_requisitos($obj_tramite){
 		$gui_slt_requisitos = file_get_contents("static/common/lst_requisitos.html");
-		print_r($gui_slt_requisitos);
+		$render = str_replace('{tramite-requisito}', $obj_tramite->requisito, $gui_slt_requisitos)
+		print $render;
 	}
 
 	function dias_disponibles($resultado) {
