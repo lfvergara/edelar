2d993810b2e673286d93d73b3358c3b626f3a247
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-24 22:46:46 -0300

    Panel ofi virtual

--- tools/getDeuda.php
+++ tools/getDeuda.php
@@ -26,7 +26,7 @@ class getDeuda {
 		$data['metodo'] = $aes->EncryptString($metodo, $secretKey);
 		$data['valor'] = $aes->EncryptString($valor, $secretKey);
 		
-		/*
+		
 		//Implementacion cURL DEUDA
 		//$ch = curl_init("https://servicios.edelar.com.ar/api_servicios_desa/wsServicios_deuda.php");
 		$ch = curl_init("https://servicios.edelar.com.ar/api_servicios/wsServicios_deuda.php");
@@ -36,20 +36,7 @@ class getDeuda {
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		$response = curl_exec($ch);
-		return $response;
-		*/
-
-		//Implementacion cURL DEUDA
-		$ch = curl_init("https://servicios.edelar.com.ar/api_servicios_desa/wsServicios_deuda.php");
-		//$ch = curl_init("https://geco.edelar.com.ar/api_geco/deuda.php");
 		
-		curl_setopt($ch, CURLOPT_TIMEOUT, 120000);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120000);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-		$response = curl_exec($ch);
 		print_r($response);exit;
 		return $response;
 	}
