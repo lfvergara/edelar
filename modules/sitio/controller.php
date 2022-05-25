55d360219a7d6a7853bb97ad4af9ee30461f7cba
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-25 14:48:55 -0300

    Panel ofi virtual

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -1634,6 +1634,7 @@ class SitioController {
 
 		$deuda = new getDatosV10();
 		$deuda_collection = $deuda->getDeudaFunction('dni', $documento);
+		print_r($documento);exit;
 		$this->view->ofivirtual_deuda($deuda_collection, $metodo);
 	}
 
