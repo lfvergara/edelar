8889fbc9e8ff829be62d5ec8d4b2c65ad85ed969
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-26 20:29:02 -0300

    Probando equifax

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -587,6 +587,7 @@ class SitioController {
 
 				eval("class OV_Gestion {};");
 				$gestion = New OV_Gestion();
+				$gestion->ov_gestion_id = 0;
 				$gestion->suministro = $nis;
 				$gestion->fecha = date('Y-m-d');
 				$gestion->dni = $dni;
