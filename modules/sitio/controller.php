ef7b7bc5f40140bd3942f21030468ea4108fe6da
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-26 19:12:19 -0300

    Probando equifax

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -791,10 +791,10 @@ class SitioController {
   	  		$tipogestion->ov_gestion->archivo_collection = $tmp_array;
 	 	}
 
+	 	$argumento = json_encode($tipogestion);
+	 	print_r($argumento);exit;
 		$ws = new postGestionGeCo();
 		$rst_cliente = $ws->postGestionFunction($argumento);
-		 	$argumento = json_encode($tipogestion);
-		 	print_r($argumento);exit;
 		if (in_array($tipo_gestion, $array_gestionescomerciales_online)) {
 	 		//$resultado = sincroniza_geco_tramite($argumento);	 		
 		 	//$resultado = sincroniza_geco_tramite_desa($argumento);
