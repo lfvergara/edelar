e156a7b142e6d96ecba54f286e7e299cf544260c
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-27 11:18:03 -0300

    Con turnero

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -926,10 +926,10 @@ class SitioController {
 	function verificar_dni($arg){
 		$fecha = date('Y-m-d');
 		$documento = $arg;
-			print_r($documento);exit;
+			
 		$select = "tp.turnopendiente_id AS ID, tp.documento AS DOCUMENTO, tp.numero AS NUMERO, tp.fecha_hasta AS FECHA_HASTA, tp.hora_solicitud AS HORA_SOLICITUD, tp.estado AS ESTADO, of.denominacion AS OFICINA, t.denominacion AS GESTION, (SELECT COUNT(*) FROM turnopendiente WHERE documento = '{$documento}' AND fecha_hasta >= '{$fecha}' AND estado = 'solicitado') AS CANTIDAD";
 		$from = "turnopendiente tp INNER JOIN oficina of ON tp.oficina = of.oficina_id INNER JOIN tramite t ON tp.tramite = t.tramite_id";
-		$where = "tp.documento = '{$documento}' AND tp.fecha_hasta >= '{$fecha}' AND tp.estado = 'solicitado'";
+		$where = "tp.documento = {$documento} AND tp.fecha_hasta >= '{$fecha}' AND tp.estado = 'solicitado'";
 		$turnopendiente_collection = CollectorCondition()->get('TurnoPendiente', $where, 4, $from, $select);
 		if (is_array($turnopendiente_collection)) {
 			$this->view->turnos_documento($turnopendiente_collection);
