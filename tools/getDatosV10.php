371258f8fdb7a2789b8f6e4ee206254f9fa31af1
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-27 21:42:18 -0300

    Probando datos cliente

--- tools/getDatosV10.php
+++ tools/getDatosV10.php
@@ -62,7 +62,7 @@ class getDatosV10 {
 		
 		
 		//Implementacion cURL DEUDA
-		$ch = curl_init("https://servicios.edelar.com.ar/api_servicios_desa/wsServicios_deuda.php");
+		$ch = curl_init("https://servicios.edelar.com.ar/api_servicios_desa/wsServicios_cliente.php");
 		//$ch = curl_init("https://servicios.edelar.com.ar/api_servicios/wsServicios_cliente.php");
 		curl_setopt($ch, CURLOPT_TIMEOUT, 120000);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 120000);
