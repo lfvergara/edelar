9c83b716c60c8cd487252376dd814b58788fdf5b
Luis Federico Vergara <hu.ce.ro@gmail.com>
2022-05-27 21:36:33 -0300

    Probando datos cliente

--- modules/sitio/controller.php
+++ modules/sitio/controller.php
@@ -1807,6 +1807,7 @@ class SitioController {
 
 		$ws = new getDatosV10();
 		$rst_cliente = $ws->getClienteFunction('dni', $documento);
+		print_r($rst_cliente);exit;
 
 		//FIX ME: COMPLETAR CON WS TRAER CLIENTE Y SUMINISTROS
 		/*
